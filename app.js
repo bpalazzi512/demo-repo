10a8a3989760f0352f5f3d4c8c07a47788feaa33
Bobby Palazzi <bobbypalazzi@Roberts-MacBook-Pro.local>
2021-03-02 17:42:34 -0500

    made it a node project

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const ejs = require('ejs')
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+
+app.get("/", (req, res) => {
+    res.render("index", {name: "bub"})
+})
+
+app.listen(3000)
